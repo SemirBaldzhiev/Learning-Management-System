80a29105f23c4cdaa1c3c2e83c669470721781ae
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-10 23:33:41 +0200

    Added changes of appsetting and program files

--- LearningManagementSystem/LearningManagementSystem.Web/Program.cs
+++ LearningManagementSystem/LearningManagementSystem.Web/Program.cs
@@ -1,5 +1,5 @@
 using LearningManagementSystem.Infrastructure.Data;
-using Microsoft.AspNetCore.Identity;
+using LearningManagementSystem.Infrastructure.Data.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +9,7 @@ builder.Services.AddDbContext<LearningSystemDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<LearningSystemDbContext>();
 builder.Services.AddControllersWithViews();
 
