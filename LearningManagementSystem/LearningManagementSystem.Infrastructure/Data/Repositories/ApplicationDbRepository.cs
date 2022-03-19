684a8c6b8ea863af2ac30db96b45dde8251dd509
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-19 22:47:10 +0200

    Added application repository

---
+++ LearningManagementSystem/LearningManagementSystem.Infrastructure/Data/Repositories/ApplicationDbRepository.cs
@@ -0,0 +1,12 @@
+﻿using LearningManagementSystem.Infrastructure.Data.Common;
+
+namespace LearningManagementSystem.Infrastructure.Data.Repositories
+{
+    public class ApplicationDbRepository : Repository, IApplicationDbRepository
+    {
+        public ApplicationDbRepository(LearningSystemDbContext context)
+        {
+            this.Context = context;
+        }
+    }
+}
---
+++ LearningManagementSystem/LearningManagementSystem.Infrastructure/Data/Repositories/IApplicationDbRepository.cs
@@ -0,0 +1,13 @@
+﻿using LearningManagementSystem.Infrastructure.Data.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Infrastructure.Data.Repositories
+{
+    public interface IApplicationDbRepository : IRepository
+    {
+    }
+}
