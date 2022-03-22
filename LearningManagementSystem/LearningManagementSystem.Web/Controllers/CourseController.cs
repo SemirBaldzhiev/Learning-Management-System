cd56637cd75c037ab3392f88b8d734bd6e5fb249
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-22 23:03:46 +0200

    Added course controller

---
+++ LearningManagementSystem/LearningManagementSystem.Web/Controllers/CourseController.cs
@@ -0,0 +1,42 @@
+﻿using LearningManagementSystem.Core.Contracts;
+using LearningManagementSystem.Core.Models.Course;
+using LearningManagementSystem.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LearningManagementSystem.Web.Controllers
+{
+    public class CourseController : BaseController
+    {
+        private readonly ICourseService courseService;
+
+        public CourseController(ICourseService _courseService)
+        {
+            courseService = _courseService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var allCourses = await courseService.All();
+
+            return View(allCourses);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateCourseViewModel model) 
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Add", "Course");
+            }
+
+            await courseService.CreateCourse(model);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
