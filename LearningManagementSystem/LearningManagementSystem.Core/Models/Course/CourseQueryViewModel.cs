dab03664dd2e0cb87d155e165c20205504f00278
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-22 23:00:42 +0200

    Added view models for course

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Course/CourseQueryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Core.Models.Course
+{
+    public class CourseQueryViewModel
+    {
+        public List<CourseViewModel> Courses { get; set; }
+    }
+}
