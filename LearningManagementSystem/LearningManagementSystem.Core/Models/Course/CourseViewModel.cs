2d8423b610c535c9445dd5955e3da175c8f82cf0
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-26 16:18:55 +0200

    Added more course view models

--- LearningManagementSystem/LearningManagementSystem.Core/Models/Course/CourseViewModel.cs
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Course/CourseViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LearningManagementSystem.Core.Models.Course
+﻿namespace LearningManagementSystem.Core.Models.Course
 {
     public class CourseViewModel
     {
---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Course/DetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using LearningManagementSystem.Core.Models.Announcement;
+using LearningManagementSystem.Core.Models.Topic;
+
+namespace LearningManagementSystem.Core.Models.Course
+{
+    public class DetailsViewModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public AnnouncementViewModel LatestAnnouncement { get; set; }
+
+        public IEnumerable<TopicViewModel> Topics { get; set; }
+    }
+}
