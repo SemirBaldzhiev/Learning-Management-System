4c1a4ebcd55860fde85e424483d26c482b804771
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-26 16:17:25 +0200

    Added announcements view models

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Announcement/AnnouncementViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LearningManagementSystem.Core.Models.Announcement
+{
+    public class AnnouncementViewModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
