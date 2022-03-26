d966e7a828aa973bf3423ca50f815a52017c4a6f
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-26 16:19:57 +0200

    Added topic view models

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Topic/TopicViewModel.cs
@@ -0,0 +1,13 @@
+﻿using LearningManagementSystem.Core.Models.Material;
+
+namespace LearningManagementSystem.Core.Models.Topic
+{
+    public class TopicViewModel
+    {
+        public string Title { get; set; }
+
+        public IEnumerable<FileMaterialViewModel> FileMaterials { get; set; }
+        public IEnumerable<TextMaterialViewModel> TextMaterials { get; set; }
+
+    }
+}
