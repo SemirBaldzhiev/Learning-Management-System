83d409f5f45228c740e18a24fe9ce2a891027a41
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-26 16:19:22 +0200

    Added materials view models

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Material/FileMaterialViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Core.Models.Material
+{
+    public class FileMaterialViewModel
+    {
+        public string FileName { get; set; }
+        public byte[] FileData { get; set; }
+
+    }
+}
---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Material/TextMaterialViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Core.Models.Material
+{
+    public class TextMaterialViewModel
+    {
+        public string Content { get; set; }
+    }
+}
