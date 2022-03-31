000f06a1cd5940afc411bc1e6ae6165c82a1025d
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-03-31 17:00:45 +0300

    Added details view model

--- LearningManagementSystem/LearningManagementSystem.Core/Models/Course/DetailsViewModel.cs
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Course/DetailsViewModel.cs
@@ -5,6 +5,7 @@ namespace LearningManagementSystem.Core.Models.Course
 {
     public class DetailsViewModel
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
 
