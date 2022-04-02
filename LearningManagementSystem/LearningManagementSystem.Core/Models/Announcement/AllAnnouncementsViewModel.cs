96d28a641e5a11aa48afa80befcec6d3a0806b56
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-04-02 23:31:54 +0300

    Added announcement view models

---
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Announcement/AllAnnouncementsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningManagementSystem.Core.Models.Announcement
+{
+    public class AllAnnouncementsViewModel
+    {
+        public List<AnnouncementViewModel> Announcements { get; set; }
+    }
+}
--- LearningManagementSystem/LearningManagementSystem.Core/Models/Announcement/AnnouncementViewModel.cs
+++ LearningManagementSystem/LearningManagementSystem.Core/Models/Announcement/AnnouncementViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class AnnouncementViewModel
     {
+        public int Id { get; set; }
         public string? Title { get; set; }
         public string? Content { get; set; }
         public DateTime LastUpdated { get; set; }
