c292819778a1d377e38e0fc0f8fe7002c29afb74
Semir Baldzhiev <semir.baldzhiev@gmail.com>
2022-04-02 23:33:33 +0300

    Added actions

--- LearningManagementSystem/LearningManagementSystem.Web/Controllers/AnnouncementController.cs
+++ LearningManagementSystem/LearningManagementSystem.Web/Controllers/AnnouncementController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearningManagementSystem.Core.Contracts;
+using LearningManagementSystem.Core.Models.Announcement;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LearningManagementSystem.Web.Controllers
 {
     public class AnnouncementController : BaseController
     {
-        public IActionResult Index()
+        private readonly IAnnouncementService service;
+
+        public AnnouncementController(IAnnouncementService _service)
+        {
+            service = _service;
+        }
+
+        public IActionResult All()
         {
             return View();
         }
+
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(CreateAnnouncementViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await service.Create(model);
+
+            return RedirectToAction(nameof(All));
+;       }
     }
 }
